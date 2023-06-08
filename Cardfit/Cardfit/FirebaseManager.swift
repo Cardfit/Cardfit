1c648fdeadb159b41adb9cc420eaf04d1362b4a1
domb <woon4910@gmail.com>
2023-06-09 03:19:57 +0900

    fix: Persistence and FirebaseManager
    영구저장소 및 파이어베이스 데이터 업로드 및 업데이트 로직 수정

--- Cardfit/Cardfit/FirebaseManager.swift
+++ Cardfit/Cardfit/FirebaseManager.swift
@@ -26,31 +26,84 @@ class FirebaseManager: NSObject {
         super.init()
     }
     
-    func fetchCardInfo(of company: CompanyList) async -> [Card] {
-        var cardList: [Card] = []
+    func downloadImage(company: CompanyList, cardNumber: String) async -> UIImage? {
         
-        let cardListRef = firestore.collection("CardList")
-        let allCardInfo = cardListRef.document("CardInfo")
-        let cardsOfSelectedCompany = allCardInfo.collection(company.rawValue)
-        
-        do {
-            let cardNumberRefs = try await cardsOfSelectedCompany.getDocuments().documents
-            
-            for cardNumberRef in cardNumberRefs {
-                do {
-                    let cardSnapshot = try await cardsOfSelectedCompany.document(cardNumberRef.documentID).getDocument()
-                    let card = try cardSnapshot.data(as: Card.self)
-                    cardList.append(card)
+        await withCheckedContinuation { continuation in
+            storage.reference(forURL: "gs://cardfit-b9d71.appspot.com/CardImage/\(company.rawValue)/\(cardNumber)").downloadURL { url, error in
+                
+                DispatchQueue.global().async {
+                    let data = try? Data(contentsOf: url!)
+                    let uiImage = UIImage(data: data!)
                     
-                } catch {
-                    print("Failed to fetch card info:", error)
+                    continuation.resume(returning: uiImage)
                 }
             }
+        }
+    }
+    
+    func getReference(of company: CompanyList) -> CollectionReference {
+        let cardListRef = firestore.collection("CardList")
+        let allCardInfo = cardListRef.document("CardInfo")
+        let selectedCompanyReference = allCardInfo.collection(company.rawValue)
+
+        return selectedCompanyReference
+    }
+
+    func fetchCardNumberList(of company: CompanyList) async -> [String] {
+        var cardNumberList: [String] = []
+        
+        do {
+            let reference = getReference(of: company)
+            let snapshots = try await reference.getDocumentsSnapShot()
+            snapshots.forEach { snapshot in
+                cardNumberList.append(snapshot.documentID)
+            }
         } catch {
-            print("Failed to fetch card number refs:", error)
+            print(error)
         }
         
-        return cardList
+        return cardNumberList
+    }
+
+    func fetchCardInfo(of company: CompanyList) async throws -> [Card] {
+        
+        let reference = getReference(of: company)
+        let snapshots = try await reference.getDocumentsSnapShot()
+        var cards = try snapshots.getData(as: Card.self)
+        
+        cards = cards.map { card in
+            var updatedCard = card
+            updatedCard.company = company.rawValue
+            return updatedCard
+        }
+        
+        return cards
+    }
+}
+
+extension CollectionReference {
+    
+    func getDocumentsSnapShot() async throws -> [QueryDocumentSnapshot] {
+        do {
+            let cardsOfSelectedCompany = try await self.getDocuments().documents
+            return cardsOfSelectedCompany
+        }
+    }
+}
+
+extension [QueryDocumentSnapshot] {
+    func getData<T: Decodable>(as type: T.Type) throws -> [T] {
+        
+        var decodedData: [T] = []
+        
+        do {
+            try self.forEach { snapshot in
+                let data = try snapshot.data(as: type)
+                decodedData.append(data)
+            }
+        }
+        
+        return decodedData
     }
 }
 
--- Cardfit/Cardfit/Persistence.swift
+++ Cardfit/Cardfit/Persistence.swift
@@ -9,12 +9,10 @@ import CoreData
 
 struct PersistenceController {
     
-    static let shared = PersistenceController()
+    static let shared = PersistenceController(inMemory: false)
+    let container = NSPersistentContainer(name: "Cardfit")
 
-    let container: NSPersistentContainer
-
-    init(inMemory: Bool = true) {
-        container = NSPersistentContainer(name: "Cardfit")
+    private init(inMemory: Bool = true) {
         
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
@@ -31,4 +29,74 @@ struct PersistenceController {
         
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
+    
+    func save() {
+        do {
+            try container.viewContext.save()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func fetchCardEntities(for company: CompanyList) throws -> [CardEntity] {
+        
+        let viewContext = container.viewContext
+        let fetchRequest: NSFetchRequest<CardEntity> = CardEntity.fetchRequest()
+        let predicate = NSPredicate(format: "company == %@", company.rawValue)
+        
+        fetchRequest.predicate = predicate
+        
+        return try viewContext.fetch(fetchRequest)
+    }
+    
+    func changeToCardEntity(from card: [Card]) {
+        
+        let viewContext = container.viewContext
+        
+        card.forEach { card in
+            
+//            if let existingCardCompany = fetchCardCompany(with: card.company, in: viewContext) {
+//
+//            } else {
+//                let cardCompanyType = CardCompanyEntity(context: viewContext)
+//                cardCompanyType.name = card.company
+//            }
+            
+            let cardEntity = CardEntity(context: viewContext)
+            
+            cardEntity.cardName = card.cardName
+            cardEntity.cardImageURL = card.cardImageURL
+            cardEntity.cardNumber = card.cardNumber
+            cardEntity.mainBenefit = card.mainBenefit
+            cardEntity.company = card.company
+        }
+    }
+    
+    func resetCardEntity() {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "CardEntity")
+
+        do {
+            let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+            try container.viewContext.execute(deleteRequest)
+            
+        } catch {
+            print("Failed to delete data in 'Card.sqlite': \(error)")
+        }
+    }
+    
+//    func fetchCardCompany(with name: String, in context: NSManagedObjectContext) -> CardCompanyEntity? {
+//
+//        let fetchRequest: NSFetchRequest<CardCompanyEntity> = CardCompanyEntity.fetchRequest()
+//
+//        fetchRequest.predicate = NSPredicate(format: "name == %@", name)
+//        fetchRequest.fetchLimit = 1
+//
+//        do {
+//            let result = try context.fetch(fetchRequest)
+//            return result.first
+//        } catch {
+//            print("Error fetching CardCompanyEntity: \(error)")
+//            return nil
+//        }
+//    }
 }
