85c6b9e4ac05a37da82d160b2e806f381e7995fb
domb <woon4910@gmail.com>
2023-06-09 03:22:36 +0900

    feat: add LoadingView, CardSearchViewModel, CardListViewModel

--- Cardfit/Cardfit/CardListViewModel.swift
+++ Cardfit/Cardfit/CardListViewModel.swift
@@ -2,7 +2,45 @@
 //  CardListViewModel.swift
 //  Cardfit
 //
-//  Created by 서동운 on 6/1/23.
+//  Created by 서동운 on 6/9/23.
 //
 
 import Foundation
+import CoreData
+
+class CardListViewModel: ObservableObject {
+    
+    @Published var cardList: [CardEntity] = []
+    
+//    @State var selectedCards: [Card]
+    
+    var company: CompanyList
+    
+    init(company: CompanyList) {
+        self.company = company
+    }
+    
+    func fetchCardList() async -> Result<[CardEntity], Error> {
+
+        do {
+            var entities = try PersistenceController.shared.fetchCardEntities(for: company)
+            
+            if entities.isEmpty {
+                let cardList = try await FirebaseManager.shared.fetchCardInfo(of: company)
+                
+                PersistenceController.shared.changeToCardEntity(from: cardList)
+                PersistenceController.shared.save()
+                
+                entities = try PersistenceController.shared.fetchCardEntities(for: company)
+            }
+            
+            return .success(entities)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    func removeCardEntity() {
+        PersistenceController.shared.resetCardEntity()
+    }
+}
