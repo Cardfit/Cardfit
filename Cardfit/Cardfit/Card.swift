bc0c4bd1cad6c783729d339ab71ae9ee094ac54c
domb <woon4910@gmail.com>
2023-06-16 13:09:17 +0900

    fix: change Card Model

--- Cardfit/Cardfit/Card.swift
+++ Cardfit/Cardfit/Card.swift
@@ -84,15 +84,19 @@ enum CompanyList: String, CaseIterable {
     }
 }
 
-struct Card: Identifiable, Hashable,  Codable {
+struct Card: Identifiable, Hashable, Codable {
     
     var id: Int?
     var cardName: String?
-    var cardNumber: String?
+    var cardNumber: String
     var cardImageURL: String?
     var domesticAnnualFee: String?
     var requiredPreviousMonthUsage: Int64?
     var mainBenefit: String?
     var company: String?
+    var benefit: Benefits?
+    var offset: CGFloat? = 0
 }
+
+typealias Benefits = [[String: [String: String]]]
 
