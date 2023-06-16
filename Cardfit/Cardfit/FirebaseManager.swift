7e274c0d42a66f86f4597b6070a43748e8fe3c26
domb <woon4910@gmail.com>
2023-06-16 13:20:44 +0900

    refactor: FirebaseManger
    필요한 함수로 통합시키고 나머지는 제거.

--- Cardfit/Cardfit/FirebaseManager.swift
+++ Cardfit/Cardfit/FirebaseManager.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestore
 import FirebaseStorage
 import CoreData
+import UIKit
 
 class FirebaseManager: NSObject {
-    
     static let shared = FirebaseManager()
     
     let storage: Storage
@@ -27,7 +28,6 @@ class FirebaseManager: NSObject {
     }
     
     func downloadImage(company: CompanyList, cardNumber: String) async -> UIImage? {
-        
         await withCheckedContinuation { continuation in
             storage.reference(forURL: "gs://cardfit-b9d71.appspot.com/CardImage/\(company.rawValue)/\(cardNumber)").downloadURL { url, error in
                 
@@ -41,69 +41,26 @@ class FirebaseManager: NSObject {
         }
     }
     
-    func getReference(of company: CompanyList) -> CollectionReference {
-        let cardListRef = firestore.collection("CardList")
-        let allCardInfo = cardListRef.document("CardInfo")
-        let selectedCompanyReference = allCardInfo.collection(company.rawValue)
-
-        return selectedCompanyReference
-    }
-
-    func fetchCardNumberList(of company: CompanyList) async -> [String] {
-        var cardNumberList: [String] = []
-        
-        do {
-            let reference = getReference(of: company)
-            let snapshots = try await reference.getDocumentsSnapShot()
-            snapshots.forEach { snapshot in
-                cardNumberList.append(snapshot.documentID)
-            }
-        } catch {
-            print(error)
-        }
-        
-        return cardNumberList
-    }
-
     func fetchCardInfo(of company: CompanyList) async throws -> [Card] {
-        
         let reference = getReference(of: company)
-        let snapshots = try await reference.getDocumentsSnapShot()
-        var cards = try snapshots.getData(as: Card.self)
-        
-        cards = cards.map { card in
-            var updatedCard = card
-            updatedCard.company = company.rawValue
-            return updatedCard
+        let snapshots = try await reference.getDocuments().documents
+        let cards = try snapshots.map { snapshot in
+            var card = try snapshot.data(as: Card.self)
+            
+            card.company = company.rawValue
+            
+            return card
         }
-        
         return cards
     }
 }
 
-extension CollectionReference {
-    
-    func getDocumentsSnapShot() async throws -> [QueryDocumentSnapshot] {
-        do {
-            let cardsOfSelectedCompany = try await self.getDocuments().documents
-            return cardsOfSelectedCompany
-        }
+extension FirebaseManager {
+    private func getReference(of company: CompanyList) -> CollectionReference {
+        let cardListRef = firestore.collection("CardList")
+        let allCardInfo = cardListRef.document("CardInfo")
+        let selectedCompanyReference = allCardInfo.collection(company.rawValue)
+        
+        return selectedCompanyReference
     }
 }
-
-extension [QueryDocumentSnapshot] {
-    func getData<T: Decodable>(as type: T.Type) throws -> [T] {
-        
-        var decodedData: [T] = []
-        
-        do {
-            try self.forEach { snapshot in
-                let data = try snapshot.data(as: type)
-                decodedData.append(data)
-            }
-        }
-        
-        return decodedData
-    }
-}
-
