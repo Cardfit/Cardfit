21cf47e1ac625b2764da4e0f8d057c23903097df
domb <woon4910@gmail.com>
2023-06-21 20:34:31 +0900

    feat: add logoHStack in WidgetContentView

--- Cardfit/CardfitWidget/WidgetContentView.swift
+++ Cardfit/CardfitWidget/WidgetContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import WidgetKit
 
 struct WidgetContentView: View {
     let card: Card
@@ -19,16 +20,45 @@ struct WidgetContentView: View {
                     .padding(.horizontal, 10)
                 
                 VStack(alignment: .leading) {
-                    
+                    TitleLabel(title: "브랜드", font: .title3)
+                    LogoHStack(brands: [])
                 }
                 .padding(15)
             }
+            
+            CardImageView(name: "CardImage")
         }
+    }
+}
+
+struct LogoHStack: View {
+    @State var brands: [String]
+    
+    var body: some View {
+        HStack(spacing: -5) {
+            ForEach(0..<10) { index in
+                //Image(brand[index])
+                
+                Circle()
+                    .foregroundColor(Color.randomColor())
+                    .frame(width: 35, height: 35)
+            }
+        }
+    }
+}
+
+extension Color {
+    static func randomColor() -> Color {
+        let red = Double.random(in: 0...1)
+        let green = Double.random(in: 0...1)
+        let blue = Double.random(in: 0...1)
+        return Color(red: red, green: green, blue: blue)
     }
 }
 
 struct WidgetContentView_Previews: PreviewProvider {
     static var previews: some View {
         WidgetContentView(card: Card())
+            .previewContext(WidgetPreviewContext(family: .systemLarge))
     }
 }
